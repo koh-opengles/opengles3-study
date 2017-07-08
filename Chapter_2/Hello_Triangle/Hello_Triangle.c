476787de2e40baed5a82f552947516718ab714e5
ryne <1273366787@qq.com>
2017-07-08 22:26:59 +0800

    第二章作业：添加gl开头的函数注释。

--- Chapter_2/Hello_Triangle/Hello_Triangle.c
+++ Chapter_2/Hello_Triangle/Hello_Triangle.c
@@ -54,7 +54,7 @@ GLuint LoadShader ( GLenum type, const char *shaderSrc )
    GLint compiled;
 
    // Create the shader object
-   shader = glCreateShader ( type );
+   shader = glCreateShader ( type ); //hlp:创建shader对象
 
    if ( shader == 0 )
    {
@@ -62,31 +62,31 @@ GLuint LoadShader ( GLenum type, const char *shaderSrc )
    }
 
    // Load the shader source
-   glShaderSource ( shader, 1, &shaderSrc, NULL );
+   glShaderSource ( shader, 1, &shaderSrc, NULL ); //hlp:加载shader源码
 
    // Compile the shader
-   glCompileShader ( shader );
+   glCompileShader ( shader ); //hlp:编译shader
 
    // Check the compile status
-   glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled );
+   glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled ); //hlp:检查shader编译错误
 
    if ( !compiled )
    {
       GLint infoLen = 0;
 
-      glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
+      glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );//hlp:获取具体的shader编译错误信息长度
 
       if ( infoLen > 1 )
       {
          char *infoLog = malloc ( sizeof ( char ) * infoLen );
 
-         glGetShaderInfoLog ( shader, infoLen, NULL, infoLog );
+         glGetShaderInfoLog ( shader, infoLen, NULL, infoLog ); //hlp:获取具体的shader编译错误信息
          esLogMessage ( "Error compiling shader:\n%s\n", infoLog );
 
          free ( infoLog );
       }
 
-      glDeleteShader ( shader );
+      glDeleteShader ( shader ); //hlp:释放shader
       return 0;
    }
 
@@ -123,43 +123,43 @@ int Init ( ESContext *esContext )
    GLint linked;
 
    // Load the vertex/fragment shaders
-   vertexShader = LoadShader ( GL_VERTEX_SHADER, vShaderStr );
-   fragmentShader = LoadShader ( GL_FRAGMENT_SHADER, fShaderStr );
+   vertexShader = LoadShader ( GL_VERTEX_SHADER, vShaderStr ); //hlp:加载定点着色器
+   fragmentShader = LoadShader ( GL_FRAGMENT_SHADER, fShaderStr ); //hlp:加载片段着色器
 
    // Create the program object
-   programObject = glCreateProgram ( );
+   programObject = glCreateProgram ( ); //hlp:创建gl程序对象，注意这里区分下 着色器对象和程序对象。一般都是一个程序对象，链接两个着色器对象
 
    if ( programObject == 0 )
    {
       return 0;
    }
 
-   glAttachShader ( programObject, vertexShader );
-   glAttachShader ( programObject, fragmentShader );
+   glAttachShader ( programObject, vertexShader ); //hlp：链接顶点着色器对象到程序对象
+   glAttachShader ( programObject, fragmentShader );//hlp:链接片段着色器对象到程序对象
 
    // Link the program
-   glLinkProgram ( programObject );
+   glLinkProgram ( programObject ); //hlp:最终链接程序对象
 
    // Check the link status
-   glGetProgramiv ( programObject, GL_LINK_STATUS, &linked );
+   glGetProgramiv ( programObject, GL_LINK_STATUS, &linked ); //hlp:获取链接状态
 
    if ( !linked )
    {
       GLint infoLen = 0;
 
-      glGetProgramiv ( programObject, GL_INFO_LOG_LENGTH, &infoLen );
+      glGetProgramiv ( programObject, GL_INFO_LOG_LENGTH, &infoLen ); //hlp:如果有错误，那么获取错误长度
 
       if ( infoLen > 1 )
       {
          char *infoLog = malloc ( sizeof ( char ) * infoLen );
 
-         glGetProgramInfoLog ( programObject, infoLen, NULL, infoLog );
+         glGetProgramInfoLog ( programObject, infoLen, NULL, infoLog );//hlp:如果有错误，那么获取具体的链接错误
          esLogMessage ( "Error linking program:\n%s\n", infoLog );
 
          free ( infoLog );
       }
 
-      glDeleteProgram ( programObject );
+      glDeleteProgram ( programObject ); //删除gl程序对象
       return FALSE;
    }
 
@@ -182,41 +182,41 @@ void Draw ( ESContext *esContext )
                          };
 
    // Set the viewport
-   glViewport ( 0, 0, esContext->width, esContext->height );
+   glViewport ( 0, 0, esContext->width, esContext->height ); //hlp:设置视口
 
    // Clear the color buffer
-   glClear ( GL_COLOR_BUFFER_BIT );
+   glClear ( GL_COLOR_BUFFER_BIT );  //hlp:清空颜色缓存
 
    // Use the program object
-   glUseProgram ( userData->programObject );
+   glUseProgram ( userData->programObject ); //hlp:使用gl程序对象
 
    // Load the vertex data
-   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
-   glEnableVertexAttribArray ( 0 );
+   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices ); //hlp:将顶点数据加载到顶点属性0
+   glEnableVertexAttribArray ( 0 ); //hlp:启用顶点属性数组
 
-   glDrawArrays ( GL_TRIANGLES, 0, 3 );
+   glDrawArrays ( GL_TRIANGLES, 0, 3 ); //hlp:开始绘制三角形，这样的话，用的顶点数据就是之前设置的。
 }
 
 void Shutdown ( ESContext *esContext )
 {
    UserData *userData = esContext->userData;
 
-   glDeleteProgram ( userData->programObject );
+   glDeleteProgram ( userData->programObject ); //删除gl程序对象
 }
 
 int esMain ( ESContext *esContext )
 {
    esContext->userData = malloc ( sizeof ( UserData ) );
 
-   esCreateWindow ( esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB );
+   esCreateWindow ( esContext, "Hello Triangle", 320, 240, ES_WINDOW_RGB );//hlp:创建gl窗口
 
    if ( !Init ( esContext ) )
    {
       return GL_FALSE;
    }
 
-   esRegisterShutdownFunc ( esContext, Shutdown );
-   esRegisterDrawFunc ( esContext, Draw );
+   esRegisterShutdownFunc ( esContext, Shutdown ); //hlp:注册关闭时的回调函数
+   esRegisterDrawFunc ( esContext, Draw );//hlp:注册gl绘制时的函数
 
    return GL_TRUE;
 }
